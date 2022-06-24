4af25c4079610133c6f5836c3443cf335b423d03
David Francisco <dfrancisco2677@gmail.com>
2022-06-24 16:03:54 -0700

    something...

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import useState from 'react';
+import { useState } from 'react';
 import GoblinForm from './GoblinForm';
 import GoblinList from './GoblinList';
 import Goblin from './Goblin';
--- src/GoblinList.js
+++ src/GoblinList.js
@@ -1,19 +1,6 @@
 import React from 'react';
 import Goblin from './Goblin';
 
-export default function GoblinList(goblin, visibleGoblins) {
-  return (
-    {
-      visibleGoblins((name, i) => 
-      
-      <div className='goblin-list quarter'
-      onClick={() => handleDeleteGoblin(goblin.name)}
-      key={goblin.name + i}
-      style={{ background: goblin.color }}>
-      <h2>{goblin.name}</h2>
-      <h3>{goblin.hp}</h3>
-
-    </div>)
-    }
-  );
+export default function GoblinList({ goblin, visibleGoblins }) {
+  return; 
 }
